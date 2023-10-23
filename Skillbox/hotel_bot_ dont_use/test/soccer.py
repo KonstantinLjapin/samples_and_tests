038ce9da2ed6af388caf0267e02b79d176aa143a
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-10-23 11:08:00 +0300

    del secret key

--- Skillbox/hotel_bot_ dont_use/test/soccer.py
+++ Skillbox/hotel_bot_ dont_use/test/soccer.py
@@ -1,9 +1,10 @@
 import json
 import requests
+
 def get_leagues():
     url = "https://livescore6.p.rapidapi.com/leagues/v2/list"
     querystring = {"Category": "soccer"}
-    headers = {        "X-RapidAPI-Key": "dca6db7200msh78a1535a7d5858ap1e3288jsnd7027d33c331",
+    headers = {        "X-RapidAPI-Key": "KEY",
         "X-RapidAPI-Host": "livescore6.p.rapidapi.com"    }
     response = requests.get(url, headers=headers, params=querystring)
     data = json.loads(response.text.encode().decode('utf-8-sig'))
