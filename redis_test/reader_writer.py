be5368e8fdbae91f313374398d9667922e76db65
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-10-26 11:45:05 +0300

    rename redis.py add fuc

--- redis_test/reader_writer.py
+++ redis_test/reader_writer.py
@@ -1,15 +1,21 @@
 import redis
 
-with redis.Redis(host='localhost', port=6379, decode_responses=True) as redis:
-    redis.hset('users_capcha_key_dict', mapping={'1234': 'Bill'})
-    redis.hset('users_captcha_flag_dict', mapping={'1234': 1})
-    print(redis.hget('users_captcha_flag_dict', '1234'))
-    redis.hset('users_captcha_flag_dict', mapping={'1234': 0})
-    print(redis.hget('users_captcha_flag_dict', '1234'))
-    redis.hset('users_capcha_key_dict', mapping={'6237': 'Hoi'})
-    print(redis.hkeys('users_capcha_key_dict'))
-    print(redis.hkeys('users_captcha_flag_dict'))
+"""
+with redis.Redis(host='localhost', port=6379, decode_responses=True) as r:
+    r.hset('users_capcha_key_dict', mapping={'1234': 'Bill'})
+    r.hset('users_captcha_flag_dict', mapping={'1234': 1})
+    print(r.hget('users_captcha_flag_dict', '1234'))
+    r.hset('users_captcha_flag_dict', mapping={'1234': 0})
+    print(r.hget('users_captcha_flag_dict', '1234'))
+    r.hset('users_capcha_key_dict', mapping={'6237': 'Hoi'})
+    print(r.hkeys('users_capcha_key_dict'))
+    print(r.hkeys('users_captcha_flag_dict'))
     #redis.hdel('users_capcha_key_dict', ['1234'])
-    print(redis.hgetall('users_capcha_key_dict'))
-    redis.hdel('users_capcha_key_dict', '1234')
-    print(redis.hgetall('users_capcha_key_dict'))
+    print(r.hgetall('users_capcha_key_dict'))
+    r.hdel('users_capcha_key_dict', '1234')
+    print(r.hgetall('users_capcha_key_dict'))
+"""
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+r.hset('users_capcha_key_dict', mapping={'1234': 'Bill'})
+print(r.hkeys('users_capcha_key_dict'))
