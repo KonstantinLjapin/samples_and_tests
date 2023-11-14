202a4f686b77dc82718e39a66184f1f1cd12765f
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-11-14 14:23:02 +0300

    add script deploy

--- Bash/auto_del_all_docker_image.sh
+++ Bash/auto_del_all_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# TODO add if else
+# TODO add if only container name
 # need chmod +
 sudo docker stop $(sudo docker ps -a -q);
 sudo docker rm $(sudo docker ps -a -q);
---
+++ Bash/start_test_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# TODO add if only container name
+# need chmod +
+sudo docker compose up;
+./auto_del_all_docker_image.sh;
