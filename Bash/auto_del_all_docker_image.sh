7d81c3f40e2d3fb423f9e0f39720528e9205c265
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-11-17 13:04:42 +0300

    add Docker dir

--- Bash/auto_del_all_docker_image.sh
+++ Bash/auto_del_all_docker_image.sh
@@ -4,3 +4,5 @@
 sudo docker stop $(sudo docker ps -a -q);
 sudo docker rm $(sudo docker ps -a -q);
 sudo docker rmi $(sudo docker  images -q);
+sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
+                  grep "^fastapi_stud-back_fastapi" | cut -d' ' -f2);
