c340d63bbff579ef42175309cae2305f17ac518b
KonstantinLjapin <lajpinkonstantin@gmail.com>
2024-02-06 20:37:26 +0300

    solutions codewars count_bits

--- simpls/capcha_for_bot/capcha_check/main.py
+++ simpls/capcha_for_bot/capcha_check/main.py
@@ -1,16 +1,34 @@
-import datetime
-import time
-from typing import Any
+
 from captcha.image import ImageCaptcha
-from random import randint
-import asyncio
-from time import monotonic
+from random import randint, choice
+import operator
+
+ops = {
+    '+' : operator.add,
+    '-' : operator.sub,
+    '*' : operator.mul,
+    '/' : operator.truediv,  # use operator.div for Python 2
+    '%' : operator.mod,
+    '^' : operator.xor,
+}
+
+
+def eval_binary_expr(op1, operator, op2):
+    op1, op2 = int(op1), int(op2)
+    return ops[operator](op1, op2)
+
+
+x = 9
+y = 9
+random_operator: str = choice(list(ops.keys()))
+print(str(x)+" ", random_operator, " "+str(y))
+eval_string: str = "{}{}{}"
+print(eval_string.format("9 ", "*", " 9"))
+print(eval_binary_expr(*(eval_string.format(str(x)+" ", random_operator, " "+str(y)).split())))
+print(eval_binary_expr(*("1 * 3".split())))
+print(eval_binary_expr(*("1 ^ 3".split())))
+print(eval_binary_expr(*("1 % 3".split())))
 threw_integer: int = randint(1000, 9999)
-
-
-def waiter_time(delay):
-    time.sleep(delay)
-    return True
 
 
 def throw_captcha(temp_integer: int):
@@ -18,4 +36,6 @@ def throw_captcha(temp_integer: int):
     data = image.generate(str(temp_integer))
 
 
-throw_captcha(64535)
+throw_captcha(threw_integer)
+
+
