9c26016c669fa43d664fc4db8dadc1dae568f252
lyapin <lajpinkonstantin@gmail.com>
2024-10-08 10:41:25 +0300

    add UPDATE

--- tests/pusto_studio/last_task_last_method.py
+++ tests/pusto_studio/last_task_last_method.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from last_tast_model import PlayerLevel
 
 
-def export_to_csv():
+async def export_to_csv():
     """Выгрузка данных игрока в CSV."""
     with open('player_levels.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['Player ID', 'Level Title', 'Is Completed', 'Received Prize'])
         player_levels = PlayerLevel.objects.all()
         for player_level in player_levels:
