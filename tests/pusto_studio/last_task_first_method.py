4b30ab63d592bb15c37b8747b177f1a610aedb52
lyapin <lajpinkonstantin@gmail.com>
2024-10-08 10:17:29 +0300

    add UPDATE

--- tests/pusto_studio/last_task_first_method.py
+++ tests/pusto_studio/last_task_first_method.py
@@ -1,11 +1,13 @@
 from last_tast_model import Player, Level, Prize, LevelPrize, PlayerLevel
 from django.utils import timezone
 
+
 def awarding_the_price(player_level: PlayerLevel) -> None:
     if player_level.completed:
         player = player_level.player
         level = player_level.level
-        prize = LevelPrize.get(Prize by level) # какакаято логика создания модели Prize
+        prize = Prize(title="YOU Awesome prize")# какакаято логика создания модели Prize
+        prize.save()
         LevelPrize.objects.create(level=level, prize=prize, received=timezone.now())
         print(f"Prize '{prize.title}' has been assigned to player '{player.player_id}' for level '{level.title}'.")
     else:
